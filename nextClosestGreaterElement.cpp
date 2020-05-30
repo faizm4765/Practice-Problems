084b38d0e6aa5987b7e58daf388bcc0b7c0c8ae5
Faiz Mohammad <40006938+faizm4765@users.noreply.github.com>
2020-05-30 17:02:21 +0530

    Create nextClosestGreaterElement.cpp

---
+++ nextClosestGreaterElement.cpp
@@ -0,0 +1,37 @@
+// This code gives next greater element for each element in an sorted array
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int nextClosestGreaterElement(int a[],int l,int r,int key){
+    int mid,index = -1;
+    while(l<=r){
+        mid = (l+r)/2;
+        if(a[mid] < key){
+            l = mid+1;
+        }else{
+            r = mid-1;
+            if(index == -1 or a[index] >= a[mid])
+                index = mid;
+        }
+    }
+    return a[index];
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    int a[n],i;
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    sort(a,a+n);   // we are sorting the array in case uses didn't enter a sorted array
+    for(i=0;i<n;i++)
+        cout<<nextClosestGreaterElement(a,i+1,n-1,a[i])<<" ";
+
+    cout<<"\n";
+    return 0;
+}
